6ba1cee7b11cdf95bc7374dfa9f75bb67887c847
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-07-22 23:27:15 +0400

    HW

---
+++ src/components/ContactForm/contactForm.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+import style from './contactForm.module.css'
+
+
+
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [userDataList, setUserDataList] = useState([]);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+   const newUserData = {
+     name: name,
+     number: number,
+   };
+
+   setUserDataList(prevUserDataList => [...prevUserDataList, newUserData]);
+
+    onAddContact(name, number);
+
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={style.form}>
+      <label htmlFor="name" className={style.label}>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+      </label>
+
+      <label htmlFor="number" className={style.label}>
+        Number
+        <input
+          type="text"
+          name="number"
+          value={number}
+          onChange={handleNumberChange}
+          required
+        />
+      </label>
+
+      <button type="submit" className={style.add}>
+        Add Contact
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm
